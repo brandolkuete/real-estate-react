e14581e31917988dc8f739d483832d77b456aa52
brandolkuete <brandolkuete@yahoo.fr>
2022-03-02 23:56:43 +0100

    modifying favicon

--- src/components/AnnounceList.js
+++ src/components/AnnounceList.js
@@ -3,33 +3,6 @@ import AnnounceService from '../service/AnnounceService'
 
 export default function AnnounceList() {
 
- /* const [announces, setAnnounces] = useState([])
-  
-
-  useEffect(() => {
-    getAnnounces();
-  },[])
-
-  const getAnnounces = () => {
-    const n_announces= []
-    AnnounceService.all_announces().then((res) => {
-      res.data.map(announce => {
-        AnnounceService.get_image(announce.image.url).then((res) => {
-          n_announces.push({
-            id: announce.id,
-            title: announce.title,
-            description: announce.description,
-            image_type: announce.image.type,
-            imageBase64String: res
-          })
-        })
-      })
-      //setAnnounces(n_announces)
-      //console.log(n_announces)
-    })
-    setAnnounces(n_announces)
-    console.log(n_announces)
-  }*/
   const [isLoading, setIsLoading] = useState(false)
   const [announces, setAnnounces] = useState([])
 
@@ -64,12 +37,6 @@ export default function AnnounceList() {
 
     useEffect(() =>{getAnnounces()} ,[getAnnounces])
 
-    /*if (isLoading) {
-        return (
-            <div>loading...</div>
-        )
-    }*/
-
   return (
     <div>
       <section className="probootstrap-section probootstrap-section-colored">
@@ -81,42 +48,21 @@ export default function AnnounceList() {
           </div>
         </div>
       </section>
-      {/*<section className="probootstrap-section probootstrap-section-sm">
-        <div className="container">
-          <div className="row">
-              {
-                ((announces.length === 0) ? 
-                  <span>Aucune annonce disponible...</span> :
-                  announces.map((announce) => 
-                  (
-                    <div className="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate" key={announce.id}>
-                      <a href="#" className="probootstrap-featured-news-box">
-                        <figure className="probootstrap-media"><img src={"data:"+announce.image.type+";base64,"+announce.imageBase64String} alt="image non disponible" className="img-responsive"/></figure>
-                        <div className="probootstrap-text">
-                          <h3>{announce.title}</h3>
-                          <p>{announce.description}</p>
-                        </div>
-                      </a>
-                      <p><a href={"/all"} className="btn btn-primary">Retour à la liste</a></p>
-                    </div>
-                  )
-                )  
-                )
-              }
-          </div>
-        </div>
-            </section>*/}
         <section className="probootstrap-section probootstrap-section-sm">
         <div className="container">
             <div className="row">{
-                isLoading ? <div>loading...</div> : (announces.length > 0) ? (
+                isLoading ? 
+                <div className="spinner-border text-success" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div> 
+                : (announces.length > 0) ? (
                     announces.map((announce) =>  (
                         <div className="col-md-4 col-sm-6 col-xs-6 col-xxs-12" key={announce.id}>
                             <a href="#" className="probootstrap-featured-news-box">
                                 <figure className="probootstrap-media"><img src={"data:"+announce.image_type+";base64,"+announce.imageBase64String} alt="image non disponible" className="img-responsive"/></figure>
                                 <div className="probootstrap-text">
                                 <h3>{announce.title}</h3>
-                                <p>{announce.description}</p>
+                                <p style={{whiteSpace:"nowrap", overflow:"hidden", textOverflow:"...."}}>{announce.description}</p>
                                 <p><a href={"/announce/"+announce.id} className="btn btn-primary">Voir les détails</a></p>
                                 </div>
                             </a>
